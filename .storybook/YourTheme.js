bc6a2c0d0835c21f2aa8262b91a478b1276fa227
ahongcredence <ahong@credence-llc.com>
2025-03-24 11:04:04 -0400

    updated storybook theme

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Fedmall STORES 2.0',
+  brandUrl: 'https://example.com',
+  brandImage: 'https://www.restricted.fedmall.mil/commerce/vertical_logo.png',
+  brandTarget: '_self',
+});
